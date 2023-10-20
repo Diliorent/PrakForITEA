4e6f0613ddbe190d54351356626eebf6291cee4f
Daniil Poznyak <92877120+Diliorent@users.noreply.github.com>
2023-10-20 20:04:04 +0300

    Допрацьована умова
    Включив "0" з хибних умов.
    Додав умову при "0" днів
    Додав загальну кількість пройдених метрів.

--- CycleZNalopichenyam/main.cpp
+++ CycleZNalopichenyam/main.cpp
@@ -5,18 +5,23 @@
 //попередній. Визначити загальну відстань, що проповзла черепаха за N днів. Число N треба запропонувати
 //ввести користувачу з клавіатури.
 int main() {
-    float initSpeedPerDay = 1.5, distance = 0;
+    float initSpeedPerDay = 1.5, distance = 0, totalDist = 0;
     int days;
     std::cout << "Hello, please enter number of days the turtle's been walking:" << std::endl;
-    while (!(std::cin >> days) || days <= 0) {
-        std::cout << "Error. Wrong number of days.\n";
+    while (!(std::cin >> days) || days < 0) {
+        std::cout << "Error: Wrong number of days.\n";
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Видалення символів, для
         // уникнення нескінченного циклу
     }
-    for (int i = 0; i < days; ++i) {
-        distance += initSpeedPerDay + 0.2 * i;
-        std::cout << "At the day " << i + 1 << ", the turtle walked: " << distance << " meters" << std::endl;
-    }
+    if (days == 0) {//
+        std::cout << "The turtle decided not to walk.\nMetres walked: 0.";
+    } else
+        for (int i = 0; i < days; ++i) {
+            distance += initSpeedPerDay + 0.2 * i;
+            totalDist += distance;
+            std::cout << "At the day " << i + 1 << ", the turtle walked: " << distance << " meters" << std::endl;
+        }
+    std::cout << "Total distance walked by the turtle: " << totalDist;
     return 0;
 }
