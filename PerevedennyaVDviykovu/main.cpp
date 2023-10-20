95db4aaae8c915f3f75ec4a5b9242e541f2f585b
Daniil Poznyak <92877120+Diliorent@users.noreply.github.com>
2023-10-20 20:29:27 +0300

    Допрацьовано, покращено
    Додана: перевірка на символи та від'ємні числа;
    Покращено: збільшено діапазон доступних чисел до максимального додатнього в int.

--- PerevedennyaVDviykovu/main.cpp
+++ PerevedennyaVDviykovu/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <limits>
 
 int main() {
-//    std::cout << "Hello, enter number between \"0\" and \"255\": " << std::endl;
-    std::cout << R"(Hello, enter number between "0" and "255": )" << std::endl;
-    int num;
+    std::cout << "Hello, I will turn your number into binary system" << std::endl;
+    int num, dva = 1;
     std::cin >> num;
-    std::cout << "Your number \"" << num << "\", is \"";
+    while (!(std::cin >> num) || num < 0) {
+        std::cout << "Error: Wrong number.\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Видалення символів, для
+        // уникнення нескінченного циклу
+    }
+    std::cout << "Your number \"" << num << "\" is: \"";
 
-    for (int i = 128; i >= 1; i = i / 2) {
+    while (num > dva)
+    {
+        dva *= 2;
+//        std::cout << dva << std::endl;
+    }
+
+    for (int i = dva; i >= 1; i = i / 2) {
         if (num >= i) {
             std::cout << 1;
             num -= i;
