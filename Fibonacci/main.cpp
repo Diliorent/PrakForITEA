b97d8a08bf75b67e7e7a4c1f2234c6c5fb00c0ad
Daniil Poznyak <92877120+Diliorent@users.noreply.github.com>
2023-10-23 14:21:59 +0300

    Виправив помилку заповнення

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -8,11 +8,8 @@
 //формулою фібоначчі[i+1] / фібоначчі[i]. Результ виведіть на екран.
 
 int main() {
-    std::array<int, 14> fibonacci;
+    std::array<int, 14> fibonacci = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377};
     std::array<int, 13> fibonacciSecond;
-    fibonacci[0] = 1, fibonacci[1] = 1, fibonacci[2] = 2, fibonacci[3] = 3, fibonacci[4] = 5,
-    fibonacci[5] = 8, fibonacci[6] = 13, fibonacci[7] = 21, fibonacci[8] = 34, fibonacci[9] = 55,
-    fibonacci[10] = 89, fibonacci[11] = 144, fibonacci[12] = 233, fibonacci[13] = 377;
     std::cout << "Fibonacci sequence:" << std::endl << "[";
     for (int i = 0; i < fibonacci.size(); ++i) {
         std::cout << fibonacci[i] << " ";
