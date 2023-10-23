6d6d4da3d703359ab2fda45b8b5f07f2dce1adea
Daniil Poznyak <92877120+Diliorent@users.noreply.github.com>
2023-10-23 13:51:34 +0300

    Update main.cpp
    Покращена візуалізація результатів.

--- PraktichniZLekciy/Random/main.cpp
+++ PraktichniZLekciy/Random/main.cpp
@@ -13,15 +13,15 @@ int main() {
 //    int nigga;
 
     for (int round = 0; round < 3; ++round) {
-        std::cout << "---------Round" << round + 1 << "-------" << std::endl;
-        std::cout << " Player1, your turn: ";
+        std::cout << "|--------Round" << round + 1 << "-------|" << std::endl;
+        std::cout << "| Player1, your turn: |";
         std::getchar();
         p1d1 = rand() % 6 + 1, p1d2 = rand() % 6 + 1;
-        std::cout << "Player1 thrown: " << p1d1 << ", " << p1d2 << ".\n";
-        std::cout << " Player2, your turn: ";
+        std::cout << "| Player1 thrown: " << p1d1 << ", " << p1d2 << "|\n";
+        std::cout << "| Player2, your turn: |";
         std::getchar();
         p2d1 = rand() % 6 + 1, p2d2 = rand() % 6 + 1;
-        std::cout << "Player2 thrown: " << p2d1 << ", " << p2d2 << ".\n";
+        std::cout << "| Player2 thrown: " << p2d1 << ", " << p2d2 << "|\n";
 
         if (p1d1 + p1d2 > p2d1 + p2d2) {
             win1 += 1;
@@ -33,14 +33,14 @@ int main() {
 //          std::cout << "Player 2 won!\n";
         }
     }
-    std::cout << "---------------------" << std::endl << "   Player1 wins: " << win1 << std::endl << "   Player2 wins: "
-              << win2 << std::endl << "---------------------" << std::endl;
+    std::cout << "|---------------------|" << std::endl << "|   Player1 wins:  " << win1 << "  |" << std::endl
+              << "|   Player2 wins:  " << win2 << "  |" << std::endl << "|---------------------|" << std::endl;
     if (win1 > win2)
-        std::cout << "    Player1 won!" << std::endl;
+        std::cout << "______Player1 won!_____" << std::endl;
     if (win1 < win2)
-        std::cout << "    Player2 won!" << std::endl;
+        std::cout << "______Player2 won!_____" << std::endl;
     if (win1 == win2)
-        std::cout << "       Draw!";
+        std::cout << "_________Draw!_________";
 
 }
 
